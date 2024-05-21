c32bbb4abc675804182d38c464042a9fc84520c3
Shristi0 <sristyshrestha93@gmail.com>
2024-05-21 14:23:55 +0545

    ABCD complete

---
+++ js/script.js
@@ -0,0 +1,86 @@
+const myFunction = () => {
+
+    $(function () {
+        $(document).scroll(function () {
+            var $nav = $(".navbar");
+            $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+        });
+    });
+    $(document).ready(function () {
+        $(".navbar").on("click", function () {
+            $(".navbar").toggleClass("active");
+        });
+    });
+    $('.brand__wrapper').owlCarousel({
+        loop: true,
+        margin: 32,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+    $('.testimonial__wrapper').owlCarousel({
+        loop: true,
+        margin: 32,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+    $('.blogs__slider').owlCarousel({
+        loop: true,
+        margin: 32,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+}
+
+const animation = () => {
+    AOS.init({
+        duration: 1500,
+    })
+}
+
+const functions = [myFunction, animation];
+
+for (let i = 0; i < functions.length; i++) {
+    try {
+        functions[i]();
+    }
+    catch (error) { }
+}
