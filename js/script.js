d5b84d41978eba92ed1f16e92427d33679aae44b
Shristi0 <sristyshrestha93@gmail.com>
2024-06-13 18:10:21 +0545

    fixes completed except banner due to gif changes

--- js/script.js
+++ js/script.js
@@ -42,7 +42,7 @@ const myFunction = () => {
                 items: 1
             },
             600: {
-                items: 2
+                items: 1
             },
             1000: {
                 items: 3
@@ -61,7 +61,7 @@ const myFunction = () => {
                 items: 1
             },
             600: {
-                items: 2
+                items: 1
             },
             1000: {
                 items: 3
