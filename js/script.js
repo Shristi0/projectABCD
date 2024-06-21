aba0b785d1aacbbf1f7785e06830dddaaaec7721
Shristi0 <sristyshrestha93@gmail.com>
2024-06-21 16:58:46 +0545

    banner completed

--- js/script.js
+++ js/script.js
@@ -11,6 +11,25 @@ const myFunction = () => {
             $(".navbar").toggleClass("active");
         });
     });
+    $('.bannerRight__slider').owlCarousel({
+        loop: true,
+        margin: 32,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 3
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
     $('.brand__wrapper').owlCarousel({
         loop: true,
         margin: 32,
@@ -26,7 +45,7 @@ const myFunction = () => {
                 items: 3
             },
             1000: {
-                items: 5
+                items: 4
             }
         }
     });
@@ -84,3 +103,28 @@ for (let i = 0; i < functions.length; i++) {
     }
     catch (error) { }
 }
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelector('.slides');
+    const totalSlides = document.querySelectorAll('.slide').length;
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+    slides.style.transform = `translateX(${-currentSlide * 100}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function previousSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Auto-slide every 3 seconds
+setInterval(nextSlide, 3000);
